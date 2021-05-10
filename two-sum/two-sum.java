e6f39a07b631d77a73523de5c1eb12dde6ded09e
한광식 <69612436+fruitson-gowid@users.noreply.github.com>
2021-05-10 16:35:37 +0900

    Time: 0 ms (100.00%), Space: 38.8 MB (85.75%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,9 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i+1; j<nums.length; j++) {
+        int[] result = new int[2];
+        for (int i=0; i<nums.length-1; i++) {
+            for (int j=i+1; j< nums.length; j++) {
                 if(nums[i] + nums[j] == target) {
-                    return Arrays.stream(new int[]{i, j}).toArray();
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
                 }
             }
         }
